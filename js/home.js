a2f923d482594a796d66a6fbcd99382db6be26b0
AlegzG <aleksandra.gaicki@gmail.com>
2025-04-03 16:37:35 +0200

    redirecting to todo-page added

--- js/home.js
+++ js/home.js
@@ -6,14 +6,17 @@ const inputUsername = document.getElementById("username");
 const inputPassword = document.getElementById("password");
 
 loginForm.addEventListener('submit', function(event){
-    
+    event.preventDefault();
+
     const enteredUsername = inputUsername.value;
     const enteredPassword = inputPassword.value;
 
     if(enteredUsername === username && enteredPassword === password){
         alert("Success!");
+
+        window.location.href = "./pages/todo-page.html";
     }else{
         alert("Incorrect username or password.");
     }
-    
+
 });
