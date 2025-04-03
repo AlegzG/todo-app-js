f836892df5db8f25dea9776dcf993f9df7c1d268
AlegzG <aleksandra.gaicki@gmail.com>
2025-04-03 16:17:36 +0200

    home.js added (test)

---
+++ js/home.js
@@ -0,0 +1,19 @@
+const username = 'admin';
+const password = '1234';
+
+const loginForm = document.getElementById("login-form");
+const inputUsername = document.getElementById("username");
+const inputPassword = document.getElementById("password");
+
+loginForm.addEventListener('submit', function(event){
+    
+    const enteredUsername = inputUsername.value;
+    const enteredPassword = inputPassword.value;
+
+    if(enteredUsername === username && enteredPassword === password){
+        alert("Success!");
+    }else{
+        alert("Incorrect username or password.");
+    }
+    
+});
