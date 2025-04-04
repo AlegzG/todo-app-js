f010efe607c616abacc1a8d41e33d1f393d96fb6
AlegzG <aleksandra.gaicki@gmail.com>
2025-04-04 13:37:19 +0200

    dinamically filling the table in todo.js

--- js/todo.js
+++ js/todo.js
@@ -8,7 +8,7 @@ const tasks = [
     },
     {
         id: 1,
-        title: "Return book",
+        title: "Return a book",
         description: "Return a book to a local library",
         created: "02.03.2025.",
         updated: "02.03.2025."
@@ -31,4 +31,69 @@ function getTasksFromLocalStorage(){
     }
 }
 
-console.log(getTasksFromLocalStorage());
+function fillTodoTable(){
+
+    const tableData = getTasksFromLocalStorage();
+    const tableBody = document.getElementById("task-list");
+
+    tableBody.innerHTML = '';
+
+    tableData.forEach( data => {
+
+        const row = document.createElement('tr');
+        
+        const idCell = document.createElement('td');
+        idCell.textContent = data.id;
+        idCell.style.display = 'none';
+
+        const titleCell = document.createElement('td');
+        titleCell.textContent = data.title;
+
+        const descriptCell = document.createElement('td');
+        descriptCell.textContent = data.description;
+
+        const createdCell = document.createElement('td');
+        createdCell.textContent = data.created;
+
+        const updatedCell = document.createElement('td');
+        updatedCell.textContent = data.updated;
+
+        const actionsCell = document.createElement('td');
+        actionsCell.style.display = 'flex';
+        actionsCell.style.gap = '10px';
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', () => editTask(data.id));
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteTask(data.id));
+
+        actionsCell.appendChild(editButton);
+        actionsCell.appendChild(deleteButton);
+
+
+        row.appendChild(idCell);
+        row.appendChild(titleCell);
+        row.appendChild(descriptCell);
+        row.appendChild(createdCell);
+        row.appendChild(updatedCell);
+        row.appendChild(actionsCell);
+
+        tableBody.appendChild(row);
+
+    });
+}
+
+function editTask(id){
+
+}
+
+function deleteTask(id){
+    
+}
+
+window.onload = function(){
+    fillTodoTable();
+}
